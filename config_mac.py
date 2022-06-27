6069bf5eaed33a4a90c1abb077fb7248586374d0
limingchen <lmc1998@qq.com>
2022-06-27 16:31:52 +0800

    work on windows, add config for win

--- config_mac.py
+++ config_mac.py
@@ -1,12 +1,13 @@
 import os
 from oddt import datasets
 
-project_dir = "D:\\AlexYu\\PAFF"
+project_dir = "/Users/o2cy/PycharmProjects/PAFF"
 
-base_dir = "D:\\AlexYu\\datasets\\dataset\\pdbbind2016"
+base_dir = "/Users/o2cy/Experiments/kdeep/datasets"
 refined_dir = os.path.join(base_dir, "refined-set")
 refined_dataset = datasets.pdbbind(home=refined_dir, default_set="refined", version=2016)
 
-core_dir = os.path.join(base_dir, "coreset")
+core_dir = os.path.join(base_dir, "core-set")
 core_dataset = datasets.pdbbind(home=core_dir, default_set="core", version=2016)
 
+uses_mk = False
