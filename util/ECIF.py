e4047964cfbcd0270984e68acdf192e887f7ea71
omotcha <omotcha2097@gmail.com>
2022-06-28 09:22:26 +0800

    add ecif (blank python file)

---
+++ util/ECIF.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+from os import listdir
+from rdkit import Chem
+from scipy.spatial.distance import cdist
+from itertools import product
+from rdkit.ML.Descriptors.MoleculeDescriptors import MolecularDescriptorCalculator
+
+
+def tmp():
+    pass
+
+
+if __name__ == '__main__':
+    tmp()
